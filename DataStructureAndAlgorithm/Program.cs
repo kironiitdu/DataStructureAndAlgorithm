7275f9818585704fb022f19b5bd85fca88f5362d
Md Farid Uddin <v-mdud@microsoft.com>
2022-07-04 22:46:55 +0800

    Add project files.

---
+++ DataStructureAndAlgorithm/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DataStructureAndAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
