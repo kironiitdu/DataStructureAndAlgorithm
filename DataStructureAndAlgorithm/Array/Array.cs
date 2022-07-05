08ac4a8c3ecb776a193aff987bd75336652826a2
Md.Farid Uddin Kiron <kironiitdu@gmail.com>
2022-07-05 09:10:39 +0800

    Update Array.cs

--- DataStructureAndAlgorithm/Array/Array.cs
+++ DataStructureAndAlgorithm/Array/Array.cs
@@ -1,13 +1,34 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DataStructureAndAlgorithm.Array
+namespace DataStructureAndAlgorithm.DsArray
 {
-    class Array
+    public class DsArray
     {
+        private int[] items;
+        private int count;
 
+        //Dynamic Array Creation
+        public DsArray(int length)
+        {
+            items = new int[length];
+        }
+
+        public void Insert(int item)
+        {
+            items[count++] = item;
+        }
+        public void Print()
+        {
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine(items[i]);
+
+            }
+            Console.ReadLine();
+        }
     }
 }
